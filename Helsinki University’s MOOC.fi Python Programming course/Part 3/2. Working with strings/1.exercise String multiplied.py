7e26c0ba3537f4483fc2f61157acd91b4121aa40
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2025-10-12 21:21:25 +0600

    update

---
+++ Helsinki University’s MOOC.fi Python Programming course/Part 3/2. Working with strings/1.exercise String multiplied.py
@@ -0,0 +1,3 @@
+work = input("Please type in a string: ")
+num = int(input("Please type in an amount: "))
+print(work * num)
---
+++ Helsinki University’s MOOC.fi Python Programming course/Part 3/2. Working with strings/2,exercise The longer string.py
@@ -0,0 +1,6 @@
+word1 = input("Please type in string 1: ")
+word2 = input("please type in string 2: ")
+
+if(len(word1) > len(word2)): print(f"{word1} is longer")
+elif(len(word2) > len(word1)): print(f"{word2} is longer")
+else: print("The strings are equally long")
