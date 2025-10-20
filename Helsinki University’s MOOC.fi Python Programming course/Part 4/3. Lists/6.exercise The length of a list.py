5cd714f272b31c22abec0b46ffca649e9c45a018
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2025-10-20 22:44:38 +0600

    update

---
+++ Helsinki University’s MOOC.fi Python Programming course/Part 4/3. Lists/6.exercise The length of a list.py
@@ -0,0 +1,9 @@
+def length(lst):
+    return len(lst)
+
+my_list = [1, 2, 3, 4, 5]
+result = length(my_list)
+print("The length is", result)
+
+result = length([1, 1, 1, 1])
+print("The length is", result)
